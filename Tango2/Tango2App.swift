6019da83622a6d47833ff4bda04b2303749b67f3
Sergei Vasilenko <ismehimeh@gmail.com>
2025-06-09 00:14:25 +0300

    Initial Commit

---
+++ Tango2/Tango2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tango2App.swift
+//  Tango2
+//
+//  Created by Sergei Vasilenko on 9.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Tango2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
