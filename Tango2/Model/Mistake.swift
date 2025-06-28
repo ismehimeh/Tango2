349741f43c3f630829f0eb07027efd85348cd451
Sergei Vasilenko <ismehimeh@gmail.com>
2025-06-28 23:18:02 +0300

    Added mistake types and their descriptions

---
+++ Tango2/Model/Mistake.swift
@@ -0,0 +1,32 @@
+//
+//  Mistake.swift
+//  Tango2
+//
+//  Created by Sergei Vasilenko on 28.06.2025.
+//
+
+enum Mistake {
+
+    case noMoreThan2
+    case signViolation(GameCellCondition.Condition)
+    case sameNumberValues
+}
+
+extension Mistake {
+    
+    var description: String {
+        switch self {
+        case .noMoreThan2:
+            return "No more than 2 🌞 and 🌚 may be next to each other, either vertically or horizontally"
+        case .signViolation(let condition):
+            switch condition {
+            case .equal:
+                return "Cells separated by an = sign must be of the same type"
+            case .opposite:
+                return "Cell separated by X sign must be of the opposite type"
+            }
+        case .sameNumberValues:
+            return "Each row (and column) must contain the same number of 🌞 and 🌚"
+        }
+    }
+}
