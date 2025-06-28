1894bc658dfd19f8a69505598b74ab6b6933ac6d
Sergei Vasilenko <ismehimeh@gmail.com>
2025-06-28 23:49:00 +0300

    Added Swift Testing

---
+++ Tango2Tests/Tango2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Tango2Tests.swift
+//  Tango2Tests
+//
+//  Created by Sergei Vasilenko on 28.06.2025.
+//
+
+import Testing
+
+struct Tango2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
