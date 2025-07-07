834e1689e9d2e9122da7e4ea79aa48f432caa839
Sergei Vasilenko <ismehimeh@gmail.com>
2025-07-07 15:38:36 +0300

    Fixed tests target problems and added new test file

---
+++ Tango2Tests/HintsTests.swift
@@ -0,0 +1,16 @@
+//
+//  HintsTests.swift
+//  Tango2Tests
+//
+//  Created by Sergei Vasilenko on 7.07.2025.
+//
+
+import Testing
+
+struct HintsTests {
+
+    @Test func test() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
