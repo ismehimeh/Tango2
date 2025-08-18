2d5634d84b6b9625834c8932fb6a1da04ff33ee6
Sergei Vasilenko <ismehimeh@gmail.com>
2025-08-18 19:40:54 +0300

    Added kind of saving level to SwiftData

--- Tango2/Tango2App.swift
+++ Tango2/Tango2App.swift
@@ -10,17 +10,35 @@ import SwiftUI
 
 @main
 struct Tango2App: App {
-    @State private var appState = AppState()
+    @State private var appState: AppState
+    private let isLevelsPersistedKey = "isLevelsPersisted"
+    private let modelContainer: ModelContainer
     
     init() {
+        let configuration = ModelConfiguration(for: Level.self)
+        modelContainer = try! ModelContainer(for: Level.self, configurations: configuration)
+        
         GameSettings.registerDefaults()
+        if !UserDefaults.standard.bool(forKey: isLevelsPersistedKey) {
+            let context = modelContainer.mainContext
+            let levels = [level1, level2, level3, level4, level5, level6, level7, level8, level9, level10, level11]
+            try! context.transaction {
+                for level in levels {
+                    context.insert(level)
+                }
+            }
+            try! context.save()
+            UserDefaults.standard.set(true, forKey: isLevelsPersistedKey)
+        }
+        
+        appState = AppState(modelContainer.mainContext)
     }
     
     var body: some Scene {
         WindowGroup {
             LevelsView(levels: appState.levels)
                 .environment(appState)
-                .modelContainer(for: [LevelCell.self])
+                .modelContainer(modelContainer)
         }
     }
 }
