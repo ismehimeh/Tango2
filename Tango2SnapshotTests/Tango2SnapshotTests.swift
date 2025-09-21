e199eea542aa4c89856b7b0331fc489f14157870
Sergei Vasilenko <ismehimeh@gmail.com>
2025-09-21 20:14:46 +0300

    Added SnapshotTesting dependency and a new target

---
+++ Tango2SnapshotTests/Tango2SnapshotTests.swift
@@ -0,0 +1,16 @@
+//
+//  Tango2SnapshotTests.swift
+//  Tango2SnapshotTests
+//
+//  Created by Sergei Vasilenko on 21.09.2025.
+//
+
+import Testing
+
+struct Tango2SnapshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
