ce13661ecb989f3e3f4cc899d915f752cfb0ff8a
Sergei Vasilenko <ismehimeh@gmail.com>
2025-09-26 14:36:06 +0300

    Test snapshot

--- Tango2SnapshotTests/Tango2SnapshotTests.swift
+++ Tango2SnapshotTests/Tango2SnapshotTests.swift
@@ -5,12 +5,25 @@
 //  Created by Sergei Vasilenko on 21.09.2025.
 //
 
+import SnapshotTesting
+import SwiftUI
 import Testing
+@testable import Tango2
 
 struct Tango2SnapshotTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @MainActor
+    @Test func tutorialFirstStage() async throws {
+        withSnapshotTesting(diffTool: .default) {
+            let view = TutorialView()
+            assertSnapshot(of: view, as: .image(drawHierarchyInKeyWindow: true,
+                                                layout: .device(config: .iPhone12)))
+        }
     }
+}
 
+extension SnapshotTestingConfiguration.DiffTool {
+  static let compare = Self {
+    "compare \"\($0)\" \"\($1)\" png: | open -f -a Preview.app"
+  }
 }
